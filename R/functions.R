ea045dd33173b46dd22cc6869caf28ce63228e9b
Charleen Konsa <charleen.konsa.232@gmail.com>
2024-05-24 11:17:21 +0300

    Optimization and fixing comments

--- R/functions.R
+++ R/functions.R
@@ -5,13 +5,12 @@ library(gridExtra)
 library(flashClust)
 sourceCpp("./src/functions.cpp")
 
-# Get k most frequent values in a column.
-# Input:
-#   - data: dataframe
-#   - column: string
-#   - k: integer, k > 0
-# Output:
-#   - vector containing k most frequent values in a column
+#' Get k most frequent values in a column.
+#'
+#' @param data: dataframe
+#' @param column: string
+#' @param k: integer, k > 0
+#' @return vector containing k most frequent values in a column
 get_most_frequent_values <- function(data, column, k) {
   most_frequent <- data %>%
                     plyr::count(column) %>%
@@ -21,13 +20,12 @@ get_most_frequent_values <- function(data, column, k) {
   return(as.vector(unlist((most_frequent))))
 }
 
-# Remove consecutive elements of the same value.
-# Input:
-#   - elements: vector
-# Output:
-#   - vector without multiple same elements in a row
-# Example:
-#   - remove_consecutive_elements(c(1, 2, 1, 1, 3)) => c(1, 2, 1, 3)
+#' Remove consecutive elements of the same value.
+#'
+#' @param elements: vector
+#' @return vector without multiple same elements in a row
+#' @examples
+#' remove_consecutive_elements(c(1, 2, 1, 1, 3)) => c(1, 2, 1, 3)
 remove_consecutive_elements <- function(elements) {
   indexes_to_remove <- c()
   for (i in seq(along = elements)[-1]) {
@@ -42,15 +40,14 @@ remove_consecutive_elements <- function(elements) {
   return(elements)
 }
 
-# Modify the vector to length of n. Remove last elements or add filler elements.
-# Input:
-#   - vector: vector
-#   - n: integer, n > 0
-# Output:
-#   - vector with length of n
-# Example:
-#   - modify_vector_to_size_n(c(1, 2, 3), 4) => c(1, 2, 3, -1)
-#   - modify_vector_to_size_n(c(1, 2, 3), 2) => c(1, 2)
+#' Modify the vector to length of n. Remove last elements or add filler elements.
+#'
+#' @param vector: vector
+#' @param n: integer, n > 0
+#' @return vector with length of n
+#' @examples
+#' modify_vector_to_size_n(c(1, 2, 3), 4) => c(1, 2, 3, -1)
+#' modify_vector_to_size_n(c(1, 2, 3), 2) => c(1, 2)
 modify_vector_to_size_n <- function(vector, n) {
   if (length(vector) >= n) {
     return(vector[1:n])
@@ -59,13 +56,12 @@ modify_vector_to_size_n <- function(vector, n) {
   }
 }
 
-# Get patients drug trajectory of length n.
-# Input:
-#   - data: dataframe
-#   - id: integer
-#   - n: integer n > 0
-# Output:
-#   - vector that is patients drug trajectory that contains drug codes.
+#' Get patients drug trajectory of length n.
+#'
+#' @param data: dataframe
+#' @param id: integer
+#' @param n: integer n > 0
+#' @return vector that is patients drug trajectory that contains drug codes.
 get_patient_drug_trajectory <- function(data, id, n) {
   filtered_data <- data %>%
     filter(SUBJECT_ID == id) %>%
@@ -75,14 +71,13 @@ get_patient_drug_trajectory <- function(data, id, n) {
   return(trajectory)
 }
 
-# Split string according to indexes.
-# Input:
-#   - string: string
-#   - indexes: numeric vector
-# Output:
-#   - vector that contains pieces of the string
-# Example:
-#   - split_into_pieces("abcdef", c(1, 2, 5)) => c("a", "bcd", "ef")
+#' Split string according to indexes.
+#'
+#' @param string: string
+#' @param indexes: numeric vector
+#' @return vector that contains pieces of the string
+#' @examples
+#' split_into_pieces("abcdef", c(1, 2, 5)) => c("a", "bcd", "ef")
 split <- function(string, indexes) {
   pieces <- c()
   for (i in seq(along = indexes)) {
@@ -97,20 +92,20 @@ split <- function(string, indexes) {
   return(pieces)
 }
 
-# Calculate distance between two ATC codes.
-# The distance is calculated by given punishments.
-# For the same elements the distance is 0.
-# If either one of the codes is missing, the distance is 1.
-# Input:
-#   - code1: string
-#   - code2: string
-#   - punishments: numeric vector
-# Output:
-#   - numeric distance between two codes
-# Example:
-#   - distance_between_codes("R03BA02", "R04BK10", c(5,4,3,2,1)) => 7
-#   - distance_between_codes("R03BA02", "R03BA02", c(5,4,3,2,1)) => 0
-#   - distance_between_codes("-1", "R03BA02", c(5,4,3,2,1)) => 1
+#' Calculate distance between two ATC codes.
+#'
+#' The distance is calculated by given punishments.
+#' For the same elements the distance is 0.
+#' If either one of the codes is missing, the distance is 1.
+#'
+#' @param code1: string
+#' @param code2: string
+#' @param punishments: numeric vector
+#' @return numeric distance between two codes
+#' @examples
+#' distance_between_codes("R03BA02", "R04BK10", c(5,4,3,2,1)) => 7
+#' distance_between_codes("R03BA02", "R03BA02", c(5,4,3,2,1)) => 0
+#' distance_between_codes("-1", "R03BA02", c(5,4,3,2,1)) => 1
 distance_between_codes <- function(code1, code2, punishments) {
   if (code1 == code2) return(0)
   if (code1 == -1 || code2 == -1) return(1)
@@ -130,12 +125,11 @@ distance_between_codes <- function(code1, code2, punishments) {
   return(distance)
 }
 
-# Get IDs of patients whose records count is in range.
-# Input:
-#   - data: dataframe
-#   - records_range: numeric vector with two values - min and max record count
-# Output:
-#   - vector that contains IDs of patients that fit into given range
+#' Get IDs of patients whose records count is in range.
+#'
+#' @param data: dataframe
+#' @param records_range: numeric vector with two values - min and max record count
+#' @return vector that contains IDs of patients that fit into given range
 get_filtered_patients <- function(data, records_range) {
   filtered_patients <- data %>%
     group_by(SUBJECT_ID) %>%
@@ -145,16 +139,15 @@ get_filtered_patients <- function(data, records_range) {
   return(filtered_patients$SUBJECT_ID)
 }
 
-# Get clusters with patients IDs.
-# Two vectors given as arguments have to have the same length.
-# Every element in the second argument shows into which cluster a patient in
-# the first argument on the same index belongs.
-# Input:
-#   - patients_ids: numeric vector
-#   - clusters_with_indexes: numeric vector
-# Output:
-#   - vector which has a list for every cluster.
-#     Lists contain patients IDs, who belong to the cluster.
+#' Get clusters with patients IDs.
+#'
+#' Two vectors given as arguments have to have the same length.
+#' Every element in the second argument shows into which cluster a patient in
+#' the first argument on the same index belongs.
+#'
+#' @param patients_ids: numeric vector
+#' @param clusters_with_indexes: numeric vector
+#' @return vector which has a list for every cluster. Lists contain patients IDs, who belong to the cluster.
 clusters_with_ids <- function(patients_ids, clusters_with_indexes) {
   clusters <- vector(mode = "list", length = max(clusters_with_indexes))
   for (i in seq(along = clusters_with_indexes)) {
@@ -164,14 +157,12 @@ clusters_with_ids <- function(patients_ids, clusters_with_indexes) {
   return(clusters)
 }
 
-# Create a plot which has a plot for each cluster.
-# Plots are showcasing occurrences of different drugs per patient.
-# Input:
-#   - data: dataframe
-#   - clusters: vector of lists, every list represents one cluster with IDs
-#   - drugs: string vector which contains drug codes
-# Output:
-#   - plot of drug occurrences in clusters
+#' Create a plot which has a plot for each cluster that showcases occurrences of different drugs per patient.
+#'
+#' @param data: dataframe
+#' @param clusters: vector of lists, every list represents one cluster with IDs
+#' @param drugs: string vector which contains drug codes
+#' @return plot of drug occurrences in clusters
 plot_clusters_drugs_count <- function(data, clusters, drugs, norm = FALSE) {
   plots <- list()
   for (i in seq(along = clusters)) {
@@ -205,44 +196,45 @@ plot_clusters_drugs_count <- function(data, clusters, drugs, norm = FALSE) {
   return(plot)
 }
 
-# Get the most frequent drug in the cluster.
-# Most frequent drug per patient is taken into account.
-# Input:
-#   - data: dataframe
-#   - cluster: numeric vector which has patients IDs
-# Output:
-#   - string of most frequent drug code in given cluster
+#' Get the most frequent drug in the cluster.
+#'
+#' The most frequent drug per patient is taken into account.
+#'
+#' @param data: dataframe
+#' @param cluster: numeric vector which has patients IDs
+#' @return string of most frequent drug code in given cluster
 get_most_frequent_drug <- function(data, cluster) {
   frequent_drugs <- c()
   for (i in seq(along = cluster)) {
     patient <- cluster[i]
     filtered_data <- data %>% filter(SUBJECT_ID == patient)
-    frequent_drug <- get_most_frequent_values(filtered_data, "DRUG_SOURCE_VALUE", 1)
+    frequent_drug <- filtered_data %>%
+                      count(DRUG_SOURCE_VALUE) %>%
+                      top_n(1, n) %>%
+                      pull(DRUG_SOURCE_VALUE)
     frequent_drugs <- append(frequent_drugs, frequent_drug)
   }
   frequency_table <- table(frequent_drugs)
   return(names(frequency_table[which.max(frequency_table)]))
 }
 
-# Gets values only from indexes in matrix.
-# Expects for matrix to be symmetrical and removes duplicates.
-# Input:
-#   - matrix: matrix
-#   - indexes: numeric vector
-# Output:
-#   - vector with matrix values where indexes intersect without duplicates
+#' Gets values from certain indexes in matrix.
+#'
+#' Expects for matrix to be symmetrical and removes duplicates.
+#'
+#' @param matrix: matrix
+#' @param indexes: numeric vector
+#' @return vector with matrix values where indexes intersect without duplicates
 get_distances <- function(matrix, indexes) {
   submatrix <- matrix[indexes, indexes]
   return(submatrix[row(submatrix) > col(submatrix)])
 }
 
-# Gets average standard deviations in clusters if there are 1 to n clusters.
-# Input:
-#   - matrix: matrix
-#   - max_cluster: integer n > 0, maximum number of clusters
-# Output:
-#   - vector with average standard deviations in clusters.
-#     On index 1 is average SD with 1 cluster, on index 2 with 2 clusters and so on.
+#' Gets average standard deviations in clusters if there are 1 to n clusters.
+#'
+#' @param matrix: matrix
+#' @param max_cluster: integer n > 0, maximum number of clusters
+#' @return vector with average standard deviations in clusters. In index 1 is average SD with 1 cluster, in index 2 with 2 clusters and so on.
 get_sd_in_clusters <- function(matrix, max_clusters = 10) {
   max_clusters <- min(max_clusters, nrow(matrix))
   average_sds <- c()
@@ -264,24 +256,21 @@ get_sd_in_clusters <- function(matrix, max_clusters = 10) {
   return(average_sds)
 }
 
-# Plots the elbow for matrix.
-# Input:
-#   - matrix: matrix
-# Output:
-#   - plot which shows average standard deviation in clusters
-#     if matrix is clustered into 1, 2, 3, ... clusters
+#' Plots the elbow for matrix.
+#'
+#' @param matrix: matrix
+#' @return plot which shows average standard deviation in clusters if matrix is clustered into 1, 2, 3, ... clusters
 plot_elbow <- function(matrix) {
   y_axis <- get_sd_in_clusters(matrix)
   x_axis <- c(seq_along(y_axis))
   plot(x_axis, y_axis, type = "l", ylab = "Standardhälve", xlab = "Klastrite arv")
 }
 
-# Count occurrences of all drugs in patients records.
-# Input:
-#   - data: dataframe
-#   - id: integer
-# Output:
-#   - dataframe which shows count of all different drugs written to patient
+#' Count occurrences of all drugs in patients records.
+#'
+#' @param data: dataframe
+#' @param id: integer
+#' @return dataframe which shows count of all different drugs written to patient
 count_all_patient_drugs <- function(data, id) {
   patient_data <- subset(data, SUBJECT_ID == id)
   drugs_count <- as.data.frame(table(patient_data$DRUG_SOURCE_VALUE))
@@ -289,23 +278,21 @@ count_all_patient_drugs <- function(data, id) {
   return(drugs_count)
 }
 
-# Get the count of a drug from data.
-# Input:
-#   - data: dataframe of drug codes with their frequencies
-#   - drug: string
-# Output:
-#   - count of the drug in data, 0 if drug is not in data
+#' Get the count of a drug from data.
+#'
+#' @param data: dataframe of drug codes with their frequencies
+#' @param drug: string
+#' @return count of the drug in data, 0 if drug is not in data
 get_drug_count <- function(data, drug) {
   drug_row <- data[data$drug_code == drug, ]
   ifelse(length(drug_row$count) == 1, return(drug_row$count), return(0))
 }
 
-# Find additional data about clusters.
-# Input:
-#   - data: dataframe
-#   - clusters: vector with lists for every cluster
-# Output:
-#   - dataframe with additional data about every cluster
+#' Find additional data about clusters.
+#'
+#' @param data: dataframe
+#' @param clusters: vector with lists for every cluster
+#' @return dataframe with additional data about every cluster
 get_additional_clusters_data <- function(data, clusters, patients_data) {
   clusters_data <- data.frame()
   max_traj_len <- length(patients_data$drugs) / length(patients_data$IDs)
@@ -324,40 +311,35 @@ get_additional_clusters_data <- function(data, clusters, patients_data) {
   return(clusters_data)
 }
 
-# Compares all patients drug trajectories with each other
-# Input:
-#   - patient_data: list with patients IDs and trajectories
-#   - punishments: numeric vector
-#   - dtw: boolean if we want to use DTW algorithm
-# Output:
-#   - symmetrical matrix which values are distances between patients trajectories
+#' Compares all patients drug trajectories with each other
+#'
+#' @param patient_data: list with patients IDs and trajectories
+#' @param punishments: numeric vector
+#' @param dtw: boolean if we want to use DTW algorithm
+#' @return symmetrical matrix which values are distances between patients trajectories
 compare_trajectories <- function(patients_data, punishments, dtw = FALSE) {
   if (dtw) return(compareDrugTrajectoriesWithDTW(patients_data, punishments))
   return(compareDrugTrajectories(patients_data, punishments))
 }
 
-# Collect data about patients.
-# Finds drug trajectory for every patient.
-# Input:
-#   - data: dataframe
-#   - ids: numeric vector
-#   - n: integer, n > 0 - length of drug trajectory
-# Output:
-#   - list with vectors named "IDs" and "drugs".
-#     "IDs" hold information about patients IDs and
-#     "drugs" contains n-length drug trajectory per patient
+#' Collect data about patients.
+#'
+#' Finds drug trajectory for every patient.
+#'
+#' @param data: dataframe
+#' @param ids: numeric vector
+#' @param n: integer, n > 0 - length of drug trajectory
+#' @return list with vectors named "IDs" and "drugs". "IDs" hold information about patients IDs and "drugs" contains n-length drug trajectory per patient
 collect_patient_data <- function(data, ids, n) {
   return(collectPatientData(data, ids, n))
 }
 
-# Converts binary to image file.
-# Input:
-#   - bin: binary code of image
-#   - name: name of the file with suitable file extension (ex. graph_1.png)
-# Output:
-#   - image file is created with given name
+#' Converts binary code to an image file.
+#'
+#' @param bin: binary code of image
+#' @param name: name of the created file
 bin_to_img <- function(bin, name) {
-  writeBin(bin, name)
+  writeBin(bin, paste(name, ".png", sep = ""))
 }
 
 only_cluster_patients <- function() {
